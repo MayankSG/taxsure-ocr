a84ac12eda76f8a1081e47305955f831efff7b9e
Mayank <mayanksaraf04@gmail.com>
2016-04-30 17:42:22 +0530

    smtp setting for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'taxsure-ocr.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true    
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV["GMAIL_USERNAME"],
+    :password       => ENV["GMAIL_PASSWORD"],
+    :enable_starttls_auto => true
+  }
+
 end
