e27867b75cd966d0b8af535713f6aecaef3c612b
Mayank <mayanksaraf04@gmail.com>
2016-04-30 16:07:57 +0530

    user confirmation email and extra params

---
+++ db/migrate/20160430095904_enable_email_confirmation_for_devise.rb
@@ -0,0 +1,12 @@
+class EnableEmailConfirmationForDevise < ActiveRecord::Migration
+  def change
+    ## Confirmable
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+
+    add_index :users, :confirmation_token, unique: true
+
+    execute("UPDATE users SET confirmed_at = NOW()")
+  end
+end
