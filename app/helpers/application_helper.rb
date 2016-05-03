2783f59ab4650fb3adcce0a7c37a2957929b9262
Mayank <mayanksaraf04@gmail.com>
2016-05-03 12:44:44 +0530

    change text-color on button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def field_class(resource, field_name)
-    if resource.errors[field_name]
-      return "error form-control".html_safe
+    if resource.errors.any?
+      return "error form-control".html_safe if resource.errors[field_name]
     else
-      return "".html_safe
+      return "green-color form-control".html_safe
     end
   end
 end
